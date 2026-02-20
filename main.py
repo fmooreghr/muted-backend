501b37f21427f6b73d8657250e8dd8b8f9fc02a6
Francisco Moore <franciscomoore@Franciscos-MacBook-Air-2.local>
2026-02-19 23:42:32 -0300

    XXX

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:8080"],
+    allow_origins=["https://muted.cl", "http://localhost:8080"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
